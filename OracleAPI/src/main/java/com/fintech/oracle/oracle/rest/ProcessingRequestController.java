6e04519ab5753055eee4c8b7eb0def626db0f61e
Sasitha Gunadasa <sasithag@zone24x7.com>
2016-12-01 16:37:44 +0530

    project setup
    added entity classes
    added processing reqeust controller

--- OracleAPI/src/main/java/com/fintech/oracle/oracle/rest/ProcessingRequestController.java
+++ OracleAPI/src/main/java/com/fintech/oracle/oracle/rest/ProcessingRequestController.java
@@ -1,4 +1,4 @@
-package com.fintech.oracle.oracle.common;
+package com.fintech.oracle.oracle.rest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
